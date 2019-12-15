f468061b2ef8f20ff431814c5c3067d3468e5df6
Bashir Egeh <b_egeh@hotmail.com>
2019-12-15 18:59:38 +0000

    update

--- bar-chart_files/bar-chart.js
+++ bar-chart_files/bar-chart.js
@@ -9,3 +9,21 @@ $('#y-name').change(function() {
     val = $('#y-name').val();
     $("#y-axis h2").text(val);
 });
+
+$('#chart-name').change(function() {
+    let val;
+    val = $('#chart-name').val();
+    $("#chart-title").text(val);
+});
+
+$('#font_color').change(function(){
+    let val;
+    val = $('#font_color').val();
+    $('#chart-title').css('color', val);
+});
+
+$('#font_size').change(function(){
+    let val;
+    val = $('#font_size').val();
+    $('#chart-title').css('font-size', val + "px");
+});
